b679f43f03d9fa593077f54ed0c17d421738b83a
Timothy Campbell <tcampbel22@gmail.com>
2025-03-19 17:04:35 +0200

    Now passing primitive type of objects as props

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -38,18 +38,24 @@ const Total = (props) => {
 
 const App = () => {
 	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
+	const part1 = {
+	  name: 'Fundamentals of React',
+	  exercises: 10
+	}
+	const part2 = {
+	  name: 'Using props to pass data',
+	  exercises: 7
+	}
+	const part3 = {
+	  name: 'State of a component',
+	  exercises: 14
+	}
   
 	return (
 	  <div>
 		<h1><Header course={course}/></h1>
-		<Content part={[part1, part2, part3]} num={[exercises1, exercises2, exercises3]}/>
-		<Total total={exercises1 + exercises2 + exercises3}/>
+		<Content part={[part1.name, part2.name, part3.name]} num={[part1.exercises, part2.exercises, part3.exercises]}/>
+		<Total total={part1.exercises + part2.exercises + part3.exercises}/>
 	  </div>
 	)
   }
