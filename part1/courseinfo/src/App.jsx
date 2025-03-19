19783efcc1073cb1dd6d1dabbd72bdbfe8e094f9
Timothy Campbell <tcampbel22@gmail.com>
2025-03-19 15:47:20 +0200

    ex1.2 completed, passed an array via the Content component to the Part component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -12,11 +12,21 @@ const Content = (props) => {
 	console.log(props)
 	return (
 		<>
-		<p>{props.part} {props.num}</p>
+		<Part part_name={props.part[0]} exercises_num={props.num[0]}/>
+		<Part part_name={props.part[1]} exercises_num={props.num[1]}/>
+		<Part part_name={props.part[2]} exercises_num={props.num[2]}/>
 		</>
 	)
 }
 
+const Part = (props) => {
+	console.log(props)
+	return (
+		<>
+		<p>{props.part_name} {props.exercises_num}</p>
+		</>
+	)
+}
 const Total = (props) => {
 	console.log(props)
 	return (
@@ -38,9 +48,7 @@ const App = () => {
 	return (
 	  <div>
 		<h1><Header course={course}/></h1>
-		<Content part={part1} num={exercises1}/>
-		<Content part={part2} num={exercises2}/>
-		<Content part={part3} num={exercises3}/>
+		<Content part={[part1, part2, part3]} num={[exercises1, exercises2, exercises3]}/>
 		<Total total={exercises1 + exercises2 + exercises3}/>
 	  </div>
 	)
