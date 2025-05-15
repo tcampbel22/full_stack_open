1fd7e2d02c09ac34af37f772bdb173b1b5961da1
Timothy Campbell <tcampbel@student.hive.fi>
2025-05-15 14:31:24 +0300

    ex2.6 comlpeted

--- part2/part2-project/src/App.jsx
+++ part2/part2-project/src/App.jsx
@@ -1,52 +1,52 @@
-import Note from './components/Note'
 import { useState } from 'react'
 
-const App = (props) => {
-	const [notes, setNotes] = useState(props.notes)
-	const [newNote, setNewNote] = useState('a new note...')
-	const [showAll, setShowAll] = useState(true);
-	
-	const addNote = (event) => {
-		event.preventDefault();
-		console.log("Button has been clicked", event.target);
-		const noteObject = {
-			content: newNote,
-			important: Math.random() < 0.5,
-			id: String(notes.length + 1)
-		}
-		setNotes(notes.concat(noteObject));
-		setNewNote('')
-	}
-	
-	const handleNewNote = (event) => {
-		event.preventDefault();
-		console.log(event.target.value);
-		setNewNote(event.target.value);
-	}
+const Person = ({person}) => { return <p> {person.name} </p> }
 
-	const notesToShow  = showAll ? notes : notes.filter(note => note.important === true)
-
-  return (
+const People = ({persons}) => {
+	return (
 	<div>
-		<h1>Notes</h1>
-		<div>
-			<button onClick={() => setShowAll(!showAll)}>
-				show {showAll ? 'important' : 'all'}
-			</button>
-		</div>
-			<ul>
-				{notesToShow.map(note => 
-					<Note key={note.id} note={note} />
-				)}
-			</ul>
-		<form onSubmit={addNote}>
-			<input 
-				value={newNote}
-				onChange={handleNewNote}
-			/>
-			<button type='submit'>Save</button>
-		</form>
-  	</div>
+		{/* <div>Debug: {persons[0].name }</div> */}
+		{persons.map(person => 
+			<Person key={person.name} person={person}/>
+		)}
+	</div>
+	)
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([{}]) //Array of people objects 
+  const [newName, setNewName] = useState('enter name...')
+
+ const addPerson = (event) => {
+	event.preventDefault();
+	console.log("Clicked add", event.target);
+	const nameObject = { name: newName }
+	console.log("new name: ", nameObject.name);
+	setPersons(persons.concat(nameObject));
+	setNewName('')
+ } 
+ 
+  const handleNewNames = (event) => {
+	event.preventDefault();
+	console.log(event.target.value);
+	setNewName(event.target.value);
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+			<label htmlFor='name'>name:</label>
+          <input id='name' name='full-name' value={newName} onChange={handleNewNames} type='text'/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+		{/* <div>debug: {newName}</div> */}
+      <h2>Numbers</h2>
+		<People persons={persons}/>
+    </div>
   )
 }
 
