0a6a762b6e94a85e2a7915c50f08b4c8456e91e5
Timothy Campbell <tcampbel22@gmail.com>
2025-07-14 15:23:43 +0300

    Added course notes app for testing

--- notes/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-const Header1 = ({ header }) => { return <h1>{header}</h1> }
-
-const Header2 = ({ header }) => { return <h2>{header}</h2> }
-
-const Header3 = ({ header }) => { return <h3>{header}</h3> }
-
-export { Header1, Header2, Header3 };
